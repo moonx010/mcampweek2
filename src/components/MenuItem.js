072e48617b23f602961ef6e1679d6da81d4f9de4
cs20200221 <moonx010@kaist.ac.kr>
2022-01-07 22:02:21 +0900

    .

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Text} from 'react-native';
 import InputSpinner from 'react-native-input-spinner';
 
-export default MenuItem = ({item}) => {
+export default MenuItem = (item) => {
     return(
-        <View>
+        <View style = {styles.container}>
+            <Text>{item.name}</Text>
             <InputSpinner
               step={1}
               skin="round"
               value={item.count}
+              style={styles.spinner}
               onChange={(num) => {
                 item.count = num;
               }}
             />
         </View>
-    )
+    );
 
-}
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginHorizontal: 12,
+    },
+    spinner: {
+        justifyContent: 'flex-end'
+    }
+})
--- src/screens/mystore/MyStoreScreen.js
+++ src/screens/mystore/MyStoreScreen.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import MenuList from '../../components/MenuList';
+
 export default function MyStoreScreen() {
 
     const [list, setList] = useState([{name: "떡볶이", cost: 1000, count: 3}, {name: "튀김", cost: 1500, count: 4}, {name: "어묵", cost: 500, count: 6}])
@@ -10,6 +12,7 @@ export default function MyStoreScreen() {
                 <Text>오늘의 매출</Text>
                 <Text>{ calculateSales(list) +" 원"}</Text>
             </View>
+            <MenuList list={list}/>
             
         </ View>
     );
